9e4b3a6f338a9427b0df33f47c92c0316947a840
oofy <oofysotry@gmail.com>
2024-09-05 16:37:16 +0900

    request.get_json is empty. but use_llm

--- html & python/app.py
+++ html & python/app.py
@@ -1,27 +1,47 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import json
 
 app = Flask(__name__)
+CORS(app)
 
 llm = Ollama(model="llama2")
 prompt = ChatPromptTemplate.from_template("You are an expert in astronomy. Answer the question. <Question>: {input}")
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
 
-@app.route('/chat', methods=['POST'])
+@app.route("/chat", methods=["POST"])
 def chat():
-    user_input = request.json.get('user_prompt')
+    print("------------------------")
 
     try:
-        response = chain.invoke({'input': user_input})
-        print(response)
-        return jsonify({'response': response})
+        # print(request.json)
+        # user_input = request.get_json(silent=True)
+        # user_input = request.get_json()
+        # prompt_text = user_input["prompt"]
+
+        if request.get_json(silent=True) == None :
+            prompt_text = "빈칸입니다."
+        else :
+            user_input = request.get_json()
+            prompt_text = user_input["prompt"]
 
     except Exception as e:
         print(f"Error: {e}")
-        return jsonify({'error': 'API 호출 중 오류 발생'}), 500
+        return jsonify({"error": str(e)}), 500
+    
 
-if __name__ == '__main__':
+    try:
+        response = chain.invoke({"input": prompt_text})
+        print(response)
+        return jsonify({"response": response})
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
