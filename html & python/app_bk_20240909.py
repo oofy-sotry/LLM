c9f8a0d7c1789ddd6da14cacef7810cfa4d1c253
oofy <oofysotry@gmail.com>
2024-09-11 14:54:03 +0900

    학습된 모델을 사용하기 위한 코드 만드는중

--- html & python/app_bk_20240909.py
+++ html & python/app_bk_20240909.py
@@ -3,20 +3,10 @@ from flask_cors import CORS
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from peft import PeftModel, PeftConfig
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import json
 
 app = Flask(__name__)
 CORS(app)
-
-# 모델과 토크나이저 로드
-config = PeftConfig.from_pretrained("jeunghyen/llama-2-ko-7b-1")
-base_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
-model = PeftModel.from_pretrained(base_model, "jeunghyen/llama-2-ko-7b-1")
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
-
-
 
 llm = Ollama(model="llama2")
 prompt = ChatPromptTemplate.from_template("You are an expert in astronomy. Answer the question. <Question>: {input}")
@@ -27,6 +17,11 @@ chain = prompt | llm | output_parser
 def chat():
 
     try:
+        # print(request.json)
+        # user_input = request.get_json(silent=True)
+        # user_input = request.get_json()
+        # prompt_text = user_input["prompt"]
+
         if request.get_json(silent=True) == None :
             prompt_text = "데이터가 들어오지 않아 질문이 없습니다."
         else :
