96e43e598b5d7c823972187e7fc0f9a7e8c63ad5
oofy <oofysotry@gmail.com>
2024-10-15 11:15:08 +0900

    속도 이슈에 대한 최적화 진행

--- LangChain_Rag/rag.2024.10.15_v1.py
+++ LangChain_Rag/rag.2024.10.15_v1.py
@@ -18,8 +18,7 @@ print(docs[0].page_content[5000:6000])
 print("----------------------------------------------------------------------------------------------------")
 
 # 2. 텍스트 분할(Text Split)
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
 
 print(len(splits))
@@ -35,12 +34,9 @@ embeddings_model = HuggingFaceEmbeddings(
     encode_kwargs={'normalize_embeddings': True}
 )
 
-print("----------------------------------------------------------------------------------------------------")
-
 # 4. 임베딩 계산
 embeddings = embeddings_model.embed_documents([split.page_content for split in splits])
 print(f"임베딩 개수: {len(embeddings)}, 첫 번째 임베딩 길이: {len(embeddings[0])}")
-print("----------------------------------------------------------------------------------------------------")
 
 # 5. Vector Store : FAISS 사용 - CPU 사용 버전 사용
 # 수정 1 - 이유 : FAISS 벡터스토어는 임베딩 벡터와 해당하는 문서를 함께 받아야 함
@@ -54,26 +50,20 @@ vectorstore = FAISS.from_embeddings(
     distance_strategy=DistanceStrategy.COSINE
 )
 
-print("----------------------------------------------------------------------------------------------------")
-
 # 6. Vector Store 저장
 vectorstore.save_local('./db/faiss')
-
-print("----------------------------------------------------------------------------------------------------")
 
 # 7. 검색
 query = "위키백과의 정책에 대해서 알려줘"
 # MMR - 다양성 고려 (lambda_mult = 0.5)
 retriever = vectorstore.as_retriever(
     search_type='mmr',
-    search_kwargs={'k': 3, 'fetch_k': 30}
+    search_kwargs={'k': 5, 'fetch_k': 50}
 )
 
 docs = retriever.get_relevant_documents(query)
 print(len(docs))
 print(docs[0])
-
-print("----------------------------------------------------------------------------------------------------")
 
 # 8. prompt 설정
 template = '''Answer the question based only on the following context:
@@ -83,70 +73,21 @@ Question: {question}
 '''
 
 # 포맷 함수
-# 수정 : 입력 텍스트 길이 조정
-# 이유 : 입력 텍스트가 너무 길면 미리 잘라서 모델이 처리할 수 있는 범위로 조정하기 위해
-def format_docs(docs, max_length=4096):
-    """
-    입력 텍스트의 길이가 너무 길 경우 모델에서 허용하는 길이로 잘라서 반환하는 함수
-    max_length는 모델에서 허용하는 최대 입력 토큰 길이
-    """
-    total_length = 0
-    selected_docs = []
-
-    for doc in docs:
-        doc_length = len(doc.page_content)
-        if total_length + doc_length > max_length:
-            break
-        selected_docs.append(doc.page_content)
-        total_length += doc_length
-
-    return '\n\n'.join(selected_docs)
+def format_docs(docs):
+    return '\n\n'.join([d.page_content for d in docs])
 
 # 9. Peft 및 토크나이저 모델 로드
 config = PeftConfig.from_pretrained("jeunghyen/llama-2-ko-7b-4")
-base_model = AutoModelForCausalLM.from_pretrained(
-    "meta-llama/Llama-2-7b-hf",
-    torch_dtype=torch.float16
-)
+base_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
 model = PeftModel.from_pretrained(base_model, "jeunghyen/llama-2-ko-7b-4")
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 
-print("----------------------------------------------------------------------------------------------------")
-
 # 10. 텍스트 추론 및 결과 생성
-# max_length로 입력 텍스트 길이를 조정
-input_text = template.format(context=format_docs(docs, max_length=4096), question=query)
+input_text = template.format(context=format_docs(docs), question=query)
 inputs = tokenizer(input_text, return_tensors='pt')
 # 수정 이유 : 답변에 대한 길이로 인한 오류 발생
-output = model.generate(**inputs, max_new_tokens=512, top_k=20, top_p=0.9)
+output = model.generate(**inputs, max_new_tokens=512)
 response = tokenizer.decode(output[0], skip_special_tokens=True)
-
-print("----------------------------------------------------------------------------------------------------")
 
 # 11. 응답 출력
 print(response)
-
-
-
-# 답변을 생성하는 속도가 너무 느림
-# 수정한 부분
-# 1. 입력 텍스트 길이 조정
-#   - text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-# 2. top_k, top_p 설정 조정
-#   - 장점 : 시간을 줄일수 있음
-#   - 단점 : 답변의 다양성 줄어듬
-#   - output = model.generate(**inputs, max_new_tokens=512, top_k=20, top_p=0.9)
-# 2.1 num_beams 값 조정(사용안함)
-#   - 탐색 범위를 줄여서 시간을 줄임
-#   - output = model.generate(**inputs, max_new_tokens=512, num_beams=1)
-# 3. FP16 (반정밀도 연산) 사용
-#   - base_model = AutoModelForCausalLM.from_pretrained(
-#       "meta-llama/Llama-2-7b-hf",
-#       torch_dtype=torch.float16
-#     )
-# 4. 문서 개수 및 검색 최적화
-#   - retriever = vectorstore.as_retriever(
-#       search_type='mmr',
-#       search_kwargs={'k': 3, 'fetch_k': 30}
-#     )
-
